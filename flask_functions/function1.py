0d4acfbad345c3ec49517d249b225bdd23bf5060
Maria Geogdzhayeva <masha.geo@gmail.com>
2020-09-20 10:47:49 +0300

    added functions folder, more work on jupyter

---
+++ flask_functions/function1.py
@@ -0,0 +1,25 @@
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+import datetime as dt
+import pytz
+import copy
+
+#declare read/write ability
+scopes = ['https://www.googleapis.com/auth/calendar']
+
+#lowk unclear how to do this well come back to it
+flow = InstalledAppFlow.from_client_secrets_file("masha_credentials.json", scopes=scopes)
+
+
+#ok back to real and applicable stuff
+#service is gonna be used EVERYWHERE its basically how we connect to the api
+service = build("calendar", "v3", credentials=credentials)
+cal_result = service.calendarList().list().execute()
+
+def get_calendar_list():
+    cnt = 0
+    calendar_list = []
+    for i in cal_result['items']:
+        calendar_list.append(str(cnt)+' '+i['summary'])
+        cnt +=1
+    return calendar_list
